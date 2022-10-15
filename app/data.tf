b4d6e4c88d6dd7496ef746370d32101cce5d4e7f
Alex Bakker <alex.bakker@energyworx.org>
2022-10-15 21:07:52 +0200

    add project_id

--- app/data.tf
+++ app/data.tf
@@ -3,6 +3,7 @@ data "google_client_config" "provider" {}
 data "google_container_cluster" "primary" {
   name = "nc-alex-bakker-gke"
   location = "europe-west4"
+  project = local.project
 }
 
 data "kubernetes_namespace" "flux_namespace" {
---
+++ app/locals.tf
@@ -0,0 +1,3 @@
+locals {
+  project = "nc-alex-bakker"
+}
