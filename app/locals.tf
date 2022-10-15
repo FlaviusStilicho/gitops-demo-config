bea2b9bef1523681ef33bb298d1c3a56f88f55d3
Alex Bakker <alex.bakker@energyworx.org>
2022-10-15 22:10:17 +0200

    add app deployment

--- app/locals.tf
+++ app/locals.tf
@@ -1,3 +1,5 @@
 locals {
   project = "nc-alex-bakker"
+  app_name = "gitops-demo-app"
+  app_port = 80
 }
--- app/main.tf
+++ app/main.tf
@@ -3,3 +3,46 @@ resource "kubernetes_namespace" "gitops_demo_namespace" {
     name = "gitops-demo-app"
   }
 }
+
+
+resource "kubernetes_deployment" "demo_app" {
+  metadata {
+    name      = local.app_name
+    namespace = kubernetes_namespace.gitops_demo_namespace.metadata.0.name
+  }
+
+  spec {
+    strategy {
+      type = "Recreate"
+    }
+    selector {
+      match_labels = {
+        app = local.app_name
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = local.app_name
+        }
+      }
+      spec {
+        container {
+          name  = local.app_name
+          image = "europe-west4-docker.pkg.dev/nc-alex-bakker/alex-sandbox/gitops-demo:${file("./app_version.txt")}"
+          liveness_probe {
+            http_get {
+              path = "/health"
+              port = "http"
+            }
+          }
+          port {
+            name           = "http"
+            container_port = local.app_port
+          }
+        }
+      }
+    }
+  }
+}
